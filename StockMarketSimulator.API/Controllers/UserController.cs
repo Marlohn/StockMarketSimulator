fe048b1a0f4e91af816b6e204ee03a101d91afa3
Marlohn Choinski <marlohn.choinski@hotmail.com>
2024-03-20 09:57:40 -0300

    add controllers

---
+++ StockMarketSimulator.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSimulator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid userId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid userId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ StockMarketSimulator.API/Controllers/WalletController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSimulator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid walletId)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendOrder(Guid walletId)
+        {
+            return Ok();
+        }
+    }
+}
